fdbb99dc0c7bd40bf3c9b7832cd99ac8ec47b64a
kdivyaa28 <kdivyaa28@gmail.com>
2016-09-02 12:15:30 +0530

    Update and rename DeleteDigit.java to DeleteDigit1.java

---
+++ DeleteDigit1.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+ +import java.util.Collections;
+ +import java.util.List;
+ +
+ +public class DeleteDigit1 {
+ +
+ +public static List splitAndGiveList(String number){
+ +int length=number.length();
+ +List list=new ArrayList();
+ +for(int i=0;i<length;i++){
+ +String newNum=number.substring(0,i)+number.substring(i+1,length);
+ +list.add(Integer.parseInt(newNum));
+ +}
+ +Collections.sort(list);
+ +return list;
+ +}
+ +
+ +public static void findLeast(String number,int noOfDigit){
+ +Integer leastNo=0;
+ +for(int i=0;i<noOfDigit;i++){
+ +leastNo=splitAndGiveList(number).get(0);
+ +number=leastNo.toString();
+ +}
+ +System.out.println(leastNo);
+ +}
+ +/**
+ +* @param args
+ +*/
+ +public static void main(String[] args) {
+ +findLeast("24631",3);
+ +
+ +}
+ +
+ +}
